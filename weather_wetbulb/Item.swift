d6184c2270f340dd9ba6fb529240dcc01b84c867
Rob <dutch-rob@users.noreply.github.com>
2026-03-23 11:25:37 -0700

    Initial Commit

---
+++ weather_wetbulb/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  weather_wetbulb
+//
+//  Created by Rob Boer on 3/23/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}

e2bdc89a6b89cca0b89e05a3b4cfcda76e184ce0
Rob <dutchrob@yahoo.com>
2026-03-23 11:25:37 -0700

    Initial Commit

---
+++ weather_wetbulbTests/weather_wetbulbTests.swift
@@ -0,0 +1,17 @@
+//
+//  weather_wetbulbTests.swift
+//  weather_wetbulbTests
+//
+//  Created by Rob Boer on 3/23/26.
+//
+
+import Testing
+@testable import weather_wetbulb
+
+struct weather_wetbulbTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
